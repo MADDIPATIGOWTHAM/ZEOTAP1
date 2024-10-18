3b70759de59ecb71df7cc6f3461d56c940f85071
MADDIPATIGOWTHAM <gowthammaddipati94@gmail.com>
2024-10-18 21:43:47 +0530

    Update weather_api.py

--- api/weather_api.py
+++ api/weather_api.py
@@ -1,7 +1,7 @@
 import requests
 
 def fetch_weather_data(city):
-    api_key = "b5679720c29f578aaae62a169240f26f"  # Replace with your API key
+    api_key = " # Replace with your API key" 
     url = f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid={api_key}&units=metric"
     response = requests.get(url)
     response.raise_for_status()  # Raise an error for bad responses
